fa257a2b3f4df7e5cf0b223eb1e8c7856c6c804c
beastben13 <beennn1@yahoo.com>
2017-03-14 14:33:07 -0400

    Working
    0-9 works

--- bluetoothlcd.ino
+++ bluetoothlcd.ino
@@ -1,30 +1,76 @@
-#include "LiquidCrystal.h"
-LiquidCrystal lcd(8,9,4,5,6,7);
-char data = 0;                //Variable for storing received data
-void setup() 
+ #include <LiquidCrystal.h>
+char data;                //Variable for storing received data
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void setup()
 {
-  lcd.begin(16,2);
   Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
   pinMode(13, OUTPUT);        //Sets digital pin 13 as output pin
+  lcd.begin(16, 2);
+  lcd.print("Ben Wong");
 }
 void loop()
 {
-  if(Serial.available() > 0)  // Send data only when you receive data:
+  if (Serial.available() > 0) // Send data only when you receive data:
   {
     data = Serial.read();      //Read the incoming data and store it into variable data
     Serial.print(data);        //Print Value inside data in Serial monitor
-    Serial.print("\n");        //New line 
-    if(data == '1')            //Checks whether value of data is equal to 1 
+    Serial.print("\n");        //New line
+    lcd.clear();
+    lcd.print("Ben Wong");
+    if (data == '1')           //Checks whether value of data is equal to 1
     {
-      digitalWrite(13, HIGH);  //If value is 1 then LED turns ON
-      lcd.setCursor(0,0);
-      lcd.print("LED is on.");
+      lcd.setCursor(0, 1);
+      lcd.print("1");  //If value is 1 then LED turns ON
     }
-    else if(data == '0')       //Checks whether value of data is equal to 0
+    else if (data == '0')      //Checks whether value of data is equal to 0
     {
-      digitalWrite(13, LOW);   //If value is 0 then LED turns OFF
-      lcd.setCursor(0,0);
-      lcd.print("LED is off.");
+      lcd.setCursor(0, 1);
+      lcd.print("0");
+    }
+    else if (data == '2')      //Checks whether value of data is equal to 0
+    {
+      lcd.setCursor(0, 1);
+      lcd.print("2");
+    }
+    else if (data == '3')      //Checks whether value of data is equal to 0
+    {
+      lcd.setCursor(0, 1);
+      lcd.print("3");
+    }
+    else if (data == '4')      //Checks whether value of data is equal to 0
+    {
+      lcd.setCursor(0, 1);
+      lcd.print("4");
+    }
+    else if (data == '5')      //Checks whether value of data is equal to 0
+    {
+      lcd.setCursor(0, 1);
+      lcd.print("5");
+    }
+    else if (data == '6')      //Checks whether value of data is equal to 0
+    {
+      lcd.setCursor(0, 1);
+      lcd.print("6");
+    }
+    else if (data == '7')      //Checks whether value of data is equal to 0
+    {
+      lcd.setCursor(0, 1);
+      lcd.print("7");
+    }
+    else if (data == '8')      //Checks whether value of data is equal to 0
+    {
+      lcd.setCursor(0, 1);
+      lcd.print("8");
+    }
+    else if (data == '9')      //Checks whether value of data is equal to 0
+    {
+      lcd.setCursor(0, 1);
+      lcd.print("9");
+    }
+    else
+    {
+      lcd.setCursor(0, 1);
+      lcd.print("Not encoded");
     }
   }
 }
